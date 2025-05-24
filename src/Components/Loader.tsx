8f54a9868cad91809cc21d85026bbd0f6791fbc2
MD AMJAD RAZA <123620419+amjadraza19@users.noreply.github.com>
2025-05-24 14:30:39 +0530

    Update Loader.tsx

--- src/Components/Loader.tsx
+++ src/Components/Loader.tsx
@@ -4,7 +4,7 @@ import IconCloud from "./magicui/icon-cloud";
 
 export function Loader() {
   return (
-    <div className="relative flex h-full w-full animate-[ping_1s_ease-in-out_1_3.5s] items-center justify-center">
+    <div className="relative flex h-full w-full animate-[ping_2s_ease-in-out_1_2.5s] items-center justify-center">
       <IconHexagon className="absolute -z-10 animate-[spin_4s_linear_infinite]" size={150} color="#64FFDA" stroke={1.25} />
       <div className=" absolute font-mono text-primaryColor font-semibold text-6xl -z-10"> A.R </div>
     </div>
